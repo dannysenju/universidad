bde9146e6661503fe2156f90fd1194e3422a833a
danny <dannysenju@gmail.com>
2018-10-06 19:22:34 -0500

    se agrega encriptacion

--- src/main/java/com/seratic/universidad/config/SecurityConfig.java
+++ src/main/java/com/seratic/universidad/config/SecurityConfig.java
@@ -1,12 +1,13 @@
 package com.seratic.universidad.config;
 
-
 import com.seratic.universidad.security.RestUnauthorizedEntryPoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +15,8 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -53,6 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
+        auth.authenticationProvider(authenticationProvider());
     }
 
     @Override
@@ -64,19 +68,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .headers().disable()
-            .csrf().disable()
-            .authorizeRequests()
+                .headers().disable()
+                .csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/failure").permitAll()
                 .antMatchers("/v2/api-docs").hasAnyAuthority("admin")
                 .antMatchers("/users/**").hasAnyAuthority("admin")
                 .anyRequest().authenticated()
                 .and()
-            .exceptionHandling()
+                .exceptionHandling()
                 .authenticationEntryPoint(restAuthenticationEntryPoint)
                 .accessDeniedHandler(restAccessDeniedHandler)
                 .and()
-            .formLogin()
+                .formLogin()
                 .loginProcessingUrl("/authenticate")
                 .successHandler(restAuthenticationSuccessHandler)
                 .failureHandler(restAuthenticationFailureHandler)
@@ -84,15 +88,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordParameter("password")
                 .permitAll()
                 .and()
-            .logout()
+                .logout()
                 .logoutUrl("/logout")
                 .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler())
                 .deleteCookies("JSESSIONID")
                 .permitAll()
                 .and()
-            .rememberMe()
+                .rememberMe()
                 .rememberMeServices(rememberMeServices)
                 .key(REMEMBER_ME_KEY)
                 .and();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-insert into users (id, login, password, first_name, last_name , e_mail, enable) VALUES (1,'seratic', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 'pepito', 'perez', 'dannysenju@gmail.com',1);
-insert into users (id, login, password, first_name, last_name, e_mail, enable) VALUES (2,'test', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 'pablo', 'parra', 'dannysenju@gmail.com',1);
+insert into users (id, login, password, first_name, last_name , e_mail, enabled) VALUES (1,'seratic', '$2a$10$X/LsmAgaZiQFIjmInKdW2eZmkh4BeURMBVDmdbj3WrzAT0eYl45m.', 'pepito', 'perez', 'dannysenju@gmail.com',1);
+insert into users (id, login, password, first_name, last_name, e_mail, enabled) VALUES (2,'test', '$2a$10$X/LsmAgaZiQFIjmInKdW2eZmkh4BeURMBVDmdbj3WrzAT0eYl45m.', 'pablo', 'parra', 'dannysenju@gmail.com',1);
 
-insert into authority (name) values ('admin');
-insert into authority (name) values ('test');
+insert into authority (id, name) values (1, 'admin');
+insert into authority (id, name) values (2, 'test');
 
 insert into users_authority (id_user, id_authority) values (1, 1);
 insert into users_authority (id_user, id_authority) values (2, 2);	
